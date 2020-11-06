4221fca3c7aad06b08310fa205398431eea33d69
Mohamed.Z <73601573+zbida-mohamed@users.noreply.github.com>
2020-11-06 11:54:57 +0100

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,9 @@
+
+var n = "Ceci est une chaine de caractere :";
+var a= 100;
+var b= 100;
+var c= 1.00;
+var d= true;
+
+
+a= (n+a)
---
+++ javascript.js
@@ -0,0 +1,27 @@
+
+
+nom=prompt("Entrez votre nom");
+
+
+prenom=prompt("Entrez votre prénom");
+
+
+
+if (confirm("Etes vous un homme ?"))
+{
+    true
+}
+else 
+{
+    false
+}
+
+
+alert (nom+prenom)
+
+
+string "Ceci est une chaine de caractere"
+
+
+
+
