1fab257dc73b5cf4086874a2f9c9857879881c5b
Mohamed.Z <73601573+zbida-mohamed@users.noreply.github.com>
2020-11-15 17:06:24 +0100

    Add files via upload

--- BOUCLESJS.js
+++ BOUCLESJS.js
@@ -26,7 +26,7 @@ console.log("fin de la boucle");
 // 
 
 // exercicce 3 
-
+var entier =0;
 var entrer = true;
  var result = 0;
  var nbr = 0
@@ -46,7 +46,7 @@ break
 }
 console.log(result,nbr);
 var moyenne = result / nbr;
- document.write("le total est ="+result+ " et la moyenne est égal à "+moyenne);
+ window.alert("le total est ="+result+ " et la moyenne est égal à "+moyenne);
 
 
 
